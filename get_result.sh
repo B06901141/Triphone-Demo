4af41ec554040edcd7e0c0e2070a45955b3b05b5
B06901141 <b06901141@ntu.edu.tw>
2020-11-03 16:56:32 +0800

    raw=true

--- get_result.sh
+++ get_result.sh
@@ -1,13 +1,11 @@
 # !/bin/bash
 
 output=rescore.0.08.log
-prefix="https://github.com/B06901141/Triphone-Demo/blob/master/"
-postfix="?raw=true"
 echo "data, label, result" > result.csv
 for i in `ls dev | sed 's/.wav//g'`; do
     x="$i\b"
     label=`cat dev.text | grep $x | cut -d ' ' -f 2- | sed 's/,//g'`
     result=`cat $output | grep $x | cut -d ' ' -f 2- | cut -d$'\n' -f 2`
     echo $i
-    echo "\"[$i]($prefix/dev/$i.wav$postfix)\",\"$label\",\"$result\"" >> result.csv
+    echo "\"[$i](./dev/$i.wav?raw=true)\",\"$label\",\"$result\"" >> result.csv
 done
